48c78082e8e23cb5ae0f7516d7aa6182c8d26742
Jesse <jessehintz44@gmail.com>
2017-05-09 14:02:43 -0600

    created copy of harvest loop for longclaw photos

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  def password_required?
+  	new_record? ? false : super
+  end
+
+  def email_required?
+  	true
+  end
+end
